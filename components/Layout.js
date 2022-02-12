6edae9759eb4c05cdf8b13e49fc6ea1d2984f995
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-02-11 21:29:12 -0300

    Configure module aliases

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
-import styles from '../styles/Layout.module.scss';
+import styles from '@/styles/Layout.module.scss';
 
 export default function Layout({ title, keywords, description, children }) {
   return (
