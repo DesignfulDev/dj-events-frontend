1d08a3c1263691cdf96bc8e50dd7f523db7ae352
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-03-15 15:12:44 -0300

    Add pagination

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,10 @@
 import qs from 'qs';
 import Layout from '@/components/Layout';
 import EventItem from '@/components/EventItem';
+import Pagination from '@/components/Pagination';
 import { API_URL } from '@/config/index';
 
-export default function EventsPage({ events }) {
+export default function EventsPage({ events, page, pageCount }) {
   return (
     <Layout>
       <h1>Events</h1>
@@ -13,25 +14,34 @@ export default function EventsPage({ events }) {
       {events.map(e => (
         <EventItem key={e.id} evt={e} />
       ))}
+
+      <Pagination page={page} pageCount={pageCount} />
     </Layout>
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ query: { page = 1 } }) {
   const query = qs.stringify(
     {
       fields: ['name', 'slug', 'date', 'time'],
       populate: 'image',
-      sort: ['date'],
+      sort: ['date:asc'],
+      pagination: {
+        page,
+      },
     },
     { encodeValuesOnly: true }
   );
 
   const res = await fetch(`${API_URL}/api/events?${query}`);
-  const { data: events } = await res.json();
+  const {
+    data: events,
+    meta: {
+      pagination: { pageCount, total },
+    },
+  } = await res.json();
 
   return {
-    props: { events },
-    revalidate: 1,
+    props: { events, page: +page, pageCount, total },
   };
 }
