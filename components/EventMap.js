1c6abb1e97d3fcc6ca466a8c88b57380c0662717
Mauricio Lacerda <mauricio.lacerda@pm.me>
2022-03-27 17:35:19 -0300

    Refactor to use maintained map API

--- components/EventMap.js
+++ components/EventMap.js
@@ -1,33 +1,43 @@
-import { useState, useEffect } from 'react';
-import {
-  GoogleMap,
-  withScriptjs,
-  withGoogleMap,
-  Marker,
-} from 'react-google-maps';
+import { useState, useEffect, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import Geocode from 'react-geocode';
 
 export default function EventMap({ evt }) {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [viewport, setViewport] = useState({
-    latitude: 40.712772,
-    longitute: -73.935242,
-    zoom: 14,
+  const zoom = 15;
+  const containerStyle = {
     width: '100%',
     height: '500px',
+  };
+
+  const [map, setMap] = useState(null);
+  const [center, setCenter] = useState({
+    lat: 40.712772,
+    lng: -73.935242,
   });
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY,
+  });
+
+  const onLoad = useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  Geocode.setApiKey(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY);
 
   useEffect(() => {
     // Get latitude & longitude from address.
     Geocode.fromAddress(evt.attributes.address).then(
       response => {
         const { lat, lng } = response.results[0].geometry.location;
-        setLat(lat);
-        setLng(lng);
-        setViewport({ ...viewport, latitude: lat, longitute: lng });
-        setLoading(false);
+        setCenter({ lat, lng });
       },
       error => {
         console.error(error);
@@ -35,27 +45,17 @@ export default function EventMap({ evt }) {
     );
   }, []);
 
-  Geocode.setApiKey(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY);
-
-  function Map() {
-    return (
-      <GoogleMap defaultCenter={{ lat, lng }} defaultZoom={viewport.zoom}>
-        <Marker key={evt.id} position={{ lat, lng }} />
-      </GoogleMap>
-    );
-  }
-  const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-  if (loading) return false;
-
-  return (
-    <div style={{ height: viewport.height, width: viewport.width }}>
-      <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
-      />
-    </div>
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={zoom}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker key={evt.id} position={{ ...center }} />
+    </GoogleMap>
+  ) : (
+    <></>
   );
 }
